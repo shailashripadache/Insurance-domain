26027383d7b0837233c5d8219a3d360b0eb9dc24
¨Shailashri <magadumshaila@gmail.com>
2023-12-14 17:13:33 +0100

    added new file

--- cypress/e2e/google-search.cy.js
+++ cypress/e2e/google-search.cy.js
@@ -1,9 +1,10 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://google.com')
-    cy.get('#L2AGLb > .QS5gu').click()
-    cy.get('#APjFqb').type("SITA.dev")
-    cy.contains('Sök på Google').click()
-    cy.title().should('eq', 'SITA.dev - Google Search')
+      cy.visit('http://demo.guru99.com/insurance/v1/index.php')
+    //cy.visit('https://google.com')
+    //cy.get('#L2AGLb > .QS5gu').click()
+    //cy.get('#APjFqb').type("SITA.dev")
+    //cy.contains('Sök på Google').click()
+    //cy.title().should('eq', 'SITA.dev - Google Search')
   })
 })
